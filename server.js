624db781083eecc1848d68848996c20c8b01068f
lucas.mourao <lucas.mourao@circularbrain.io>
2021-06-22 20:47:54 -0300

    feat: set discord theme as main screen

--- server.js
+++ server.js
@@ -20,55 +20,71 @@ let users = [{username: 'Alisha [BOT]', userIcon: '🤖', socketId: '0'}];
 io.on("connection", socket => {
   console.log(`Socket conectado: ${socket.id}`);
 
+  // Envia ao novo usuário conectado todas as mensagens anteriores e a lista de usuários online.
   socket.emit("previousMessages", messages);
   socket.emit("users", users);
   
-  socket.on("sendMessage", data => {
-    console.log(data);
+  // Quando esse usuário conectado enviar uma mensagem
+  socket.on("sendMessage", async data => {
+    // Salva a mensagem
     messages.push(data);
 
-    let userExists = false;
-    for(let i = 0; i < users.length; i++) {
-        if (users[i].username == data.author) {
-            userExists = true;
-            break;
-        }
-    }
+    // Verifica se esse usuário existe na lista de usuários online e adiciona e tals
+    userVerify(data, socket);
 
-    if(userExists === false){
-      users.push({username: data.author, userIcon: data.userIcon, socketId: socket.id});
-    }
-
-    io.emit("users", users);
+    // Envia a mensagem desse novo usuário para os demais usuários online.
     socket.broadcast.emit("receivedMessage", data);
   });
 
-  socket.on('disconnect', data => {
-    console.log('############### ', users);
-    if (data === 'transport close') {
-      
-      let userOffline;
-      users.forEach((user) => {
-        if (user.socketId === socket.id) {
-          userOffline = user;
-        }
-      });
+    // Se esse usuário se desconectar
+    socket.on('disconnect', data => {
 
-      users = users.filter(item => item.socketId !== socket.id)
-
-      if(userOffline){
-        io.emit("receivedMessage", {
-          author: users[0].username,
-          message: `${userOffline.username} saiu 🙁`,
-          userIcon: users[0].userIcon,
-          ip: '000.000.000.000'
+      if (data === 'transport close') {
+        // Pega o usuário da lista de usuários online.
+        let userOffline;
+        users.forEach((user) => {
+          if (user.socketId === socket.id) {
+            userOffline = user;
+          }
         });
-      }
 
-      io.emit("users", users);
-    }
+        // Atualiza lista de usuários online.
+        users = users.filter(item => item.socketId !== socket.id)
+
+        // O BOT irá disparar uma mensagem para todos dizendo que fulano saiu.
+        if(userOffline){
+          io.emit("receivedMessage", {
+            author: users[0].username,
+            message: `${userOffline.username} saiu 🙁`,
+            userIcon: users[0].userIcon,
+            ip: '000.000.000.000'
+          });
+        }
+
+        // Envia a nova lista de usuários online para todos.
+        io.emit("users", users);
+      }
+  });
 });
-});
+
+const userVerify = async (data, socket) => {
+  // Verifica se esse usuário já existe na lista de usuários online.
+  let userExists = false;
+  for(let i = 0; i < users.length; i++) {
+      if (users[i].username == data.author) {
+          userExists = true;
+          break;
+      }
+  }
+  
+  // Se não existir, adiciona
+  if(userExists === false){
+    users.push({username: data.author, userIcon: data.userIcon, socketId: socket.id});
+  }
+  
+  // Envia a nova lista de usuários online para todos.
+  io.emit("users", users);
+};
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT);
